e2518bc510be0aee6c8305476bcc5d0a3c612a9e
josedamara <josedamara0211@gmail.com>
2024-10-06 16:58:41 -0700

    Added specification for CashFlow class

--- src/main/model/CashFlow.java
+++ src/main/model/CashFlow.java
@@ -1,9 +1,74 @@
 package model;
 
+// A class representing a cash flow with a status (credit/debit), category,
+// date, time, and description
 public class CashFlow {
-    int other;
+    // EFFECTS: constructs a CashFlow object with a specified status,
+    // category, date, time, and description
+    public CashFlow(String status, String category, String date, String time, String description) {
+        // STUB
+    }
 
-    public CashFlow() {
-        other = 4;
+    // MODIFIES: this
+    // EFFECTS: changes the status of the CashFlow object to credit
+    public void setAsCredit() {
+        // STUB
+    }
+
+    // MODIFIES: this
+    // EFFECTS: changes the status of the CashFlow object to debit
+    public void setAsDebit() {
+        // STUB
+    }
+
+    // MODIFIES: this
+    // EFFECTS: changes the category of the CashFlow object to inputted
+    // category
+    public void setCategory(String category) {
+        // STUB
+    }
+
+    // MODIFIES: this
+    // EFFECTS: changes the date of the CashFlow object to inputted date
+    public void setDate(String date) {
+        // STUB
+    }
+
+    // MODIFIES: this
+    // EFFECTS: changes the time of the CashFlow object to inputted time
+    public void setTime(String time) {
+        // STUB
+    }
+
+    // MODIFIES: this
+    // EFFECTS: changes the description of the CashFlow object to inputted
+    // description
+    public void setDescription(String description) {
+        // STUB
+    }
+
+    // EFFECTS: returns the status of the CashFlow object
+    public String getStatus() {
+        return ""; // STUB
+    }
+
+    // EFFECTS: returns the category of the CashFlow object
+    public String getCategory() {
+        return ""; // STUB
+    }
+
+    // EFFECTS: returns the date of the CashFlow object
+    public String getDate() {
+        return ""; // STUB
+    }
+
+    // EFFECTS: returns the time of the CashFlow object
+    public String getTime() {
+        return ""; // STUB
+    }
+
+    // EFFECTS: returns the description of the CashFlow object
+    public String getDescription() {
+        return ""; // STUB
     }
 }
