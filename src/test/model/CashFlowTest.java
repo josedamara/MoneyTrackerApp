f477e29e56a95eea43efa1305a32650c77fac41e
josedamara <josedamara0211@gmail.com>
2024-10-08 00:07:34 -0700

    Added tests related to account field in CashFlowTest class

--- src/test/model/CashFlowTest.java
+++ src/test/model/CashFlowTest.java
@@ -10,13 +10,14 @@ public class CashFlowTest {
     
     @BeforeEach
     void runBefore() {
-        testCashFlow = new CashFlow("credit", "Sport", "2024/01/01",
+        testCashFlow = new CashFlow("credit", "ScotiaBank", "Sport", "2024/01/01",
         "22:40", "Buying a new basketball");
     }
 
     @Test
     void testConstructor() {
         assertEquals(testCashFlow.getStatus(), "credit");
+        assertEquals(testCashFlow.getAccount(), "ScotiaBank");
         assertEquals(testCashFlow.getCategory(), "Sport");
         assertEquals(testCashFlow.getDate(), "2024/01/01");
         assertEquals(testCashFlow.getTime(), "22:40");
@@ -38,6 +39,14 @@ public class CashFlowTest {
         assertEquals(testCashFlow.getStatus(), "debit");
         testCashFlow.setAsDebit();
         assertEquals(testCashFlow.getStatus(), "debit");
+    }
+
+    @Test
+    void testSetAccount() {
+        testCashFlow.setAccount("RBC");
+        assertEquals(testCashFlow.getAccount(), "RBC");
+        testCashFlow.setAccount("RBC");
+        assertEquals(testCashFlow.getAccount(), "RBC");
     }
 
     @Test
