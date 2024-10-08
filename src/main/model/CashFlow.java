ad306f31549140bc7ba76fe48ccd6e71d941fbde
josedamara <josedamara0211@gmail.com>
2024-10-07 23:32:26 -0700

    Added implementation for the CashFlow class and added a little more specification in the setCategory method

--- src/main/model/CashFlow.java
+++ src/main/model/CashFlow.java
@@ -3,35 +3,46 @@ package model;
 // A class representing a cash flow with a status (credit/debit), category,
 // date, time, and description
 public class CashFlow {
+    private String status;
+    private String category;
+    private String date;
+    private String time;
+    private String description;
+
     // REQUIRES: 
     // 1900 <= year <= 2100, 01 <= month <= 12, and date constraint should
     // follow the constraint of specific month. 
     // 00 <= hour <= 23, 00 <= minutes <= 59.
-    // category should be exist in the ListOfCategory.
+    // category must exist in the ListOfCategory.
     // EFFECTS: constructs a CashFlow object with a specified status,
     // category, date (formatted as YYYY/MM/DD), time (formatted as HH:MM),
     // and description
     public CashFlow(String status, String category, String date, String time, String description) {
-        // STUB
+        this.status = status;
+        this.category = category;
+        this.date = date;
+        this.time = time;
+        this.description = description;
     }
 
     // MODIFIES: this
     // EFFECTS: changes the status of the CashFlow object to credit
     public void setAsCredit() {
-        // STUB
+        this.status = "credit";
     }
 
     // MODIFIES: this
     // EFFECTS: changes the status of the CashFlow object to debit
     public void setAsDebit() {
-        // STUB
+        this.status = "debit";
     }
 
+    // REQUIRES: inputted category must exist in the ListOfCategory
     // MODIFIES: this
     // EFFECTS: changes the category of the CashFlow object to inputted
     // category
     public void setCategory(String category) {
-        // STUB
+        this.category = category;
     }
 
     // REQUIRES: 1900 <= year <= 2100, 01 <= month <= 12, and date constraint
@@ -39,45 +50,45 @@ public class CashFlow {
     // MODIFIES: this
     // EFFECTS: changes the date of the CashFlow object to inputted date
     public void setDate(String date) {
-        // STUB
+        this.date = date;
     }
 
     // REQUIRES: 00 <= hour <= 23, 00 <= minutes <= 59
     // MODIFIES: this
     // EFFECTS: changes the time of the CashFlow object to inputted time
     public void setTime(String time) {
-        // STUB
+        this.time = time;
     }
 
     // MODIFIES: this
     // EFFECTS: changes the description of the CashFlow object to inputted
     // description
     public void setDescription(String description) {
-        // STUB
+        this.description = description;
     }
 
     // EFFECTS: returns the status of the CashFlow object
     public String getStatus() {
-        return ""; // STUB
+        return this.status;
     }
 
     // EFFECTS: returns the category of the CashFlow object
     public String getCategory() {
-        return ""; // STUB
+        return this.category;
     }
 
     // EFFECTS: returns the date of the CashFlow object
     public String getDate() {
-        return ""; // STUB
+        return this.date;
     }
 
     // EFFECTS: returns the time of the CashFlow object
     public String getTime() {
-        return ""; // STUB
+        return this.time;
     }
 
     // EFFECTS: returns the description of the CashFlow object
     public String getDescription() {
-        return ""; // STUB
+        return this.description;
     }
 }
