3850959d27446729fc9acd72166f9ac4ebeb6389
josedamara <josedamara0211@gmail.com>
2024-10-10 12:15:06 -0700

    Added general specification of MoneyTrackerApp class in ui

--- src/main/ui/MoneyTrackerApp.java
+++ src/main/ui/MoneyTrackerApp.java
@@ -1,5 +1,54 @@
 package ui;
 
+// A money tracker application that allows user to track their cash flows
+// by adding it to the list, specifying its detail, editing it, and deleting
+// it if needed
 public class MoneyTrackerApp {
+    // EFFECTS: creates an instance of the MoneyTracker application
+    public MoneyTrackerApp() {
+
+    }
+
+    // EFFECTS: runs an interactive main menu to help user navigate what
+    // they want to do
+    public void handleMainMenu() {
+
+    }
     
+    // EFFECTS: runs an interactive cashflows menu that allows user to navigate
+    // to add, edit, or delete any cash flow
+    public void handleCashflowsMenu() {
+
+    }
+
+    // EFFECTS: runs an interactive categories menu that allows user to navigate
+    // to debitCategories menu or creditCategories menu
+    public void handleCategoriesMenu() {
+
+    }
+
+    // EFFECTS: runs an interactive debitCategories menu that allows user to add
+    // or delete any debit category and view a list of it
+    public void handleDebitCategoriesMenu() {
+
+    }
+
+    // EFFECTS: runs an interactive creditCategories menu that allows user to add
+    // or delete any credit category and view a list of it
+    public void handleCreditCategoriesMenu() {
+
+    }
+
+    // EFFECTS: runs an interactive accounts menu that allows user to add or
+    // or delete any account and view a list of it
+    public void handleAccountsMenu() {
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: marks the application as stopped and prints some closing
+    // messages
+    public void quitMoneyTrackerApp() {
+        
+    }
 }
