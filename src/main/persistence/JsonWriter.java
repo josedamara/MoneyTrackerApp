1f0df4d30b45f25036611c3c4470aff2518fc2e6
josedamara <josedamara0211@gmail.com>
2024-10-22 18:12:31 -0700

    Added implementation for JsonWriter

--- src/main/persistence/JsonWriter.java
+++ src/main/persistence/JsonWriter.java
@@ -4,38 +4,45 @@ import model.MoneySummary;
 
 import java.io.*;
 
+import org.json.JSONObject;
+
 // Referenced from the JsonSerialization Demo
 // https://github.students.cs.ubc.ca/CPSC210/JsonSerializationDemo
 
 // A class representing a writer that writes moneysummary into a JSON file
 public class JsonWriter {
+    private static final int TAB = 4;
+    private PrintWriter writer;
+    private String destination;
+
     // EFFECTS: construct writer object to write to destination file
     public JsonWriter(String destination) {
-        // STUB
+        this.destination = destination;
     }
 
     // MODIFIES: this
     // EFFECTS: opens the writer; throws FileNotFoundException error when the
     // specified file cannot be opened for writing
     public void open() throws FileNotFoundException {
-        // STUB
+        writer = new PrintWriter(new File(destination));
     }
 
     // MODIFIES: this
     // EFFECTS: writes moneysummary into a JSON file
     public void write(MoneySummary ms) {
-        // STUB
+        JSONObject json = ms.toJson();
+        saveToFile(json.toString(TAB));
     }
 
     // MODIFIES: this
     // EFFECTS: closes the writer
     public void close() {
-        // STUB
+        writer.close();
     }
 
     // MODIFIES: this
     // EFFECTS: writes string to the file
     public void saveToFile(String json) {
-        // STUB
+        writer.print(json);
     }
 }
