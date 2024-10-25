cdfd7b731f87d15292580a4fdcd3263d1f8374a9
josedamara <josedamara0211@gmail.com>
2024-10-25 16:35:54 -0700

    Added class-level specification for Writable interface

--- src/main/persistence/Writable.java
+++ src/main/persistence/Writable.java
@@ -2,6 +2,7 @@ package persistence;
 
 import org.json.JSONObject;
 
+// Interface for JsonWriter and JsonReader
 public interface Writable {
     // EFFECTS: returns this as JSON Object
     JSONObject toJson();
